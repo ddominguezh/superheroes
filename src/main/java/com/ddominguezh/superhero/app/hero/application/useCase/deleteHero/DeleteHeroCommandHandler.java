09725cecd77587f9a0661e8e68d96af7dd18dad7
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 21:21:08 +0100

    create command handler

---
+++ src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/deleteHero/DeleteHeroCommandHandler.java
@@ -0,0 +1,20 @@
+package com.ddominguezh.superhero.app.hero.application.useCase.deleteHero;
+
+import javax.annotation.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ddominguezh.superhero.shared.domain.bus.command.CommandHandler;
+
+@ManagedBean
+public class DeleteHeroCommandHandler implements CommandHandler<DeleteHeroCommand>{
+
+	@Autowired
+	private HeroDeleter deleter;
+	
+	@Override
+	public void handle(DeleteHeroCommand command) {
+		deleter.invoke(command);
+	}
+
+}
