ee412001e92e5dacc9fab6ae1599eb5187bdb270
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:24:34 +0100

    run use case test

--- src/test/java/com/ddominguezh/superhero/app/gender/GenderTestSuite.java
+++ src/test/java/com/ddominguezh/superhero/app/gender/GenderTestSuite.java
@@ -3,9 +3,11 @@ package com.ddominguezh.superhero.app.gender;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+import com.ddominguezh.superhero.app.gender.application.useCase.findGender.GenderFinderTest;
+
 @SelectClasses(
 		{
-
+			GenderFinderTest.class
 		}
 	)
 @Suite
