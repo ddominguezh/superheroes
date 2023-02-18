d1b20cc5b19ce8b2add90ac1f5d486db94f44b33
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 08:20:44 +0100

    query class

---
+++ src/main/java/com/ddominguezh/superhero/app/color/application/useCase/findColor/FindColorQuery.java
@@ -0,0 +1,7 @@
+package com.ddominguezh.superhero.app.color.application.useCase.findColor;
+
+import com.ddominguezh.superhero.shared.domain.bus.query.Query;
+
+public class FindColorQuery implements Query{
+
+}
