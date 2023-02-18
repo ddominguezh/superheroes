42345f87e9a6f697ac99d88a5cbaadbcd0ba8837
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:28:15 +0100

    call repository fuction

--- src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/GenderFinder.java
+++ src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/GenderFinder.java
@@ -1,13 +1,18 @@
 package com.ddominguezh.superhero.app.gender.application.useCase.findGender;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ddominguezh.superhero.app.gender.domain.repository.GenderRepository;
 
 @Service
 public class GenderFinder {
 
-	public FindGenderResponse invoke(FindGenderQuery findGenderQuery) {
-		// TODO Auto-generated method stub
-		return null;
+	@Autowired
+	private GenderRepository repository;
+	
+	public FindGenderResponse invoke(FindGenderQuery query) {
+		return FindGenderResponse.from(repository.findAll());
 	}
 
 }
