3f1266c9272963b4d2862854c3b195ead337ecb9
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 18:13:35 +0100

    add hibernate support and call the function all

--- src/main/java/com/ddominguezh/superhero/app/gender/infrastructure/repository/H2GenderRepository.java
+++ src/main/java/com/ddominguezh/superhero/app/gender/infrastructure/repository/H2GenderRepository.java
@@ -2,18 +2,26 @@ package com.ddominguezh.superhero.app.gender.infrastructure.repository;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ddominguezh.superhero.app.gender.domain.Gender;
 import com.ddominguezh.superhero.app.gender.domain.repository.GenderRepository;
+import com.ddominguezh.superhero.shared.infrastructure.hibernate.HibernateRepository;
 
 @Repository
-public class H2GenderRepository implements GenderRepository {
+@Transactional("transaction_manager")
+public class H2GenderRepository extends HibernateRepository<Gender> implements GenderRepository {
+
+	public H2GenderRepository(@Qualifier("session_factory") SessionFactory sessionFactory) {
+		super(sessionFactory, Gender.class);
+	}
 
 	@Override
 	public List<Gender> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return all();
 	}
 
 }
