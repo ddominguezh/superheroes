cc39735bc67e4e6d10bc2bf83041d4af48fd2fd8
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:19:07 +0100

    create response object

---
+++ src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/FindGenderResponse.java
@@ -0,0 +1,7 @@
+package com.ddominguezh.superhero.app.gender.application.useCase.findGender;
+
+import com.ddominguezh.superhero.shared.domain.bus.query.Response;
+
+public class FindGenderResponse implements Response {
+
+}
