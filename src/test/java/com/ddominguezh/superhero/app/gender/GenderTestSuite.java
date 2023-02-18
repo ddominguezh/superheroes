454c4aa466975043d2eaca90cd2a10aea3a3a2e9
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 14:39:44 +0100

    Add gender test suite

---
+++ src/test/java/com/ddominguezh/superhero/app/gender/GenderTestSuite.java
@@ -0,0 +1,14 @@
+package com.ddominguezh.superhero.app.gender;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@SelectClasses(
+		{
+
+		}
+	)
+@Suite
+public class GenderTestSuite {
+
+}
