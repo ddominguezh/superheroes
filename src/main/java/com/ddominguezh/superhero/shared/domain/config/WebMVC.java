06acee94dd9265d972b70a84fd2ecc851c3d77c2
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 07:55:09 +0100

    remove all configuration

--- src/main/java/com/ddominguezh/superhero/shared/domain/config/WebMVC.java
+++ src/main/java/com/ddominguezh/superhero/shared/domain/config/WebMVC.java
@@ -1,40 +1,17 @@
 package com.ddominguezh.superhero.shared.domain.config;
 
-import java.util.Locale;
-
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"com.ddominguezh.superhero.app"})
 public class WebMVC implements WebMvcConfigurer {
-	
-	/*
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry
-			.addMapping("/**")
-			.allowedOrigins("*", ""
-					", "http://localhost:8065", "http://localhost:5000", "http://turismoderonda.es", "http://www.turismoderonda.es")
-			.allowedMethods("*")
-			.allowCredentials(true)
-			.allowedHeaders("*");
-	}
-	*/
 	
 	@Bean
 	public FilterRegistrationBean<CharacterEncodingFilter> filterRegistrationBean() {
