f9bc903550e26a4d2836d2822fbf933b0ce1765c
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 18:23:15 +0100

    create ping test ko

--- src/test/java/com/ddominguezh/superhero/app/gender/GenderTestSuite.java
+++ src/test/java/com/ddominguezh/superhero/app/gender/GenderTestSuite.java
@@ -4,11 +4,13 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 import com.ddominguezh.superhero.app.gender.application.useCase.findGender.GenderFinderTest;
+import com.ddominguezh.superhero.app.gender.infrastructure.controller.GenderGetControllerTest;
 import com.ddominguezh.superhero.app.gender.infrastructure.repository.H2GenderRepositoryTest;
 
 @SelectClasses(
 		{
 			GenderFinderTest.class,
+			GenderGetControllerTest.class,
 			H2GenderRepositoryTest.class
 		}
 	)
