35ac567c6ef1a149904a76863c0346684242e8bd
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 20:28:26 +0100

    create get_hero test ko

--- src/test/java/com/ddominguezh/superhero/app/hero/infrastructure/controller/HeroGetControllerTest.java
+++ src/test/java/com/ddominguezh/superhero/app/hero/infrastructure/controller/HeroGetControllerTest.java
@@ -1,5 +1,9 @@
 package com.ddominguezh.superhero.app.hero.infrastructure.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +14,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.ddominguezh.superhero.app.gender.application.useCase.findGender.FindGenderResponse;
+import com.ddominguezh.superhero.app.hero.application.useCase.findOneHero.FindOneHeroQuery;
+import com.ddominguezh.superhero.app.hero.application.useCase.findOneHero.FindOneHeroResponse;
 import com.ddominguezh.superhero.shared.SuperheroApplication;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,4 +41,17 @@ public class HeroGetControllerTest {
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 	
+	@Test
+	public void get_hero() throws UnsupportedEncodingException, Exception {
+		String response = mockMvc.perform(MockMvcRequestBuilders.get("/hero/" + heroId)
+				.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		FindOneHeroResponse hero = gson.fromJson(response, FindOneHeroResponse.class);
+		assertEquals(heroId, hero.getId());
+	}
 }
