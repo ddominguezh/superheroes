1179ccba692cbdba307db76eb11d41f6a990ac6f
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 19:35:35 +0100

    throw exception when hero not found

--- src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/findOneHero/OneHeroFinder.java
+++ src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/findOneHero/OneHeroFinder.java
@@ -1,8 +1,12 @@
 package com.ddominguezh.superhero.app.hero.application.useCase.findOneHero;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ddominguezh.superhero.app.hero.domain.Hero;
+import com.ddominguezh.superhero.app.hero.domain.exception.HeroNotFoundException;
 import com.ddominguezh.superhero.app.hero.domain.repository.HeroRepository;
 import com.ddominguezh.superhero.app.hero.domain.valueObject.HeroId;
 
@@ -13,7 +17,11 @@ public class OneHeroFinder {
 	private HeroRepository repository;
 	
 	public FindOneHeroResponse invoke(FindOneHeroQuery query) {
-		return FindOneHeroResponse.from(this.repository.findById(HeroId.create(query.getId())));
+		Optional<Hero> hero = this.repository.findById(HeroId.create(query.getId()));
+		if(hero.isEmpty()) {
+			throw new HeroNotFoundException();
+		}
+		return FindOneHeroResponse.from(hero.get());
 	}
 
 }
