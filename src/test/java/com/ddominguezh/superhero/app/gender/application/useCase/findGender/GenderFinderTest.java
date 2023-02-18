9020ac35717d2a225dda2797e75e7197134d5f4d
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:52:54 +0100

    create test get_all_genders ko

--- src/test/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/GenderFinderTest.java
+++ src/test/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/GenderFinderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.ddominguezh.superhero.app.color.application.useCase.findColor.FindColorQuery;
-import com.ddominguezh.superhero.app.color.application.useCase.findColor.FindColorResponse;
-import com.ddominguezh.superhero.app.color.domain.Color;
 import com.ddominguezh.superhero.app.gender.domain.Gender;
+import com.ddominguezh.superhero.app.gender.domain.GenderMother;
 import com.ddominguezh.superhero.app.gender.domain.repository.GenderRepository;
 import com.ddominguezh.superhero.shared.SuperheroApplication;
 
@@ -45,4 +44,17 @@ public class GenderFinderTest {
 		assertEquals(0, response.getSize());
 	}
 	
+	@Test
+	public void get_all_genders() {
+		List<Gender> genders = GenderMother.randomGenders();
+		when(repository.findAll()).thenReturn(genders);
+		FindGenderResponse response = finder.invoke(new FindGenderQuery());
+		assertEquals(genders.size(), response.getSize());
+		for(int i = 0 ; i < response.getSize() ; i++ ) {
+			Gender gender = genders.get(i);
+			GenderResponse genderResponse = response.getGenders().get(i);
+			assertEquals(gender.id(), genderResponse.getId());
+			assertEquals(gender.name(), genderResponse.getName());
+		}
+	}
 }
