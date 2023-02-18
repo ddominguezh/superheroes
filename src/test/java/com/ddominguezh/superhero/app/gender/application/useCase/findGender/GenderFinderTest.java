a8504cd937bf8868b4c627df1a5d13137df7f566
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:24:12 +0100

    complete get_empty_gender_list test

--- src/test/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/GenderFinderTest.java
+++ src/test/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/GenderFinderTest.java
@@ -1,5 +1,41 @@
 package com.ddominguezh.superhero.app.gender.application.useCase.findGender;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ddominguezh.superhero.app.color.application.useCase.findColor.FindColorQuery;
+import com.ddominguezh.superhero.app.color.application.useCase.findColor.FindColorResponse;
+import com.ddominguezh.superhero.app.color.domain.Color;
+import com.ddominguezh.superhero.app.gender.domain.Gender;
+import com.ddominguezh.superhero.app.gender.domain.repository.GenderRepository;
+import com.ddominguezh.superhero.shared.SuperheroApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = SuperheroApplication.class)
 public class GenderFinderTest {
 
+	@InjectMocks
+	private GenderFinder finder;
+	
+	@Mock
+	private GenderRepository repository;
+	
+	@Test
+	public void get_empty_gender_list() {
+		when(repository.findAll()).thenReturn(new ArrayList<Gender>());
+		FindGenderResponse response = finder.invoke(new FindGenderQuery());
+		assertEquals(0, response.getSize());
+	}
+	
 }
