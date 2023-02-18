9b7faa6b2d54b37f6483cf58064ebd12144e83a1
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:18:47 +0100

    create query object

---
+++ src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/FindGenderQuery.java
@@ -0,0 +1,7 @@
+package com.ddominguezh.superhero.app.gender.application.useCase.findGender;
+
+import com.ddominguezh.superhero.shared.domain.bus.query.Query;
+
+public class FindGenderQuery implements Query {
+
+}
