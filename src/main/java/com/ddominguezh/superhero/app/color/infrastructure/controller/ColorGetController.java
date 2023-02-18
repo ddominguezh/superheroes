57a0612cea2cf0eb5fa5f3d65c5953cd7b6d7e1d
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 14:25:56 +0100

    clear imports

--- src/main/java/com/ddominguezh/superhero/app/color/infrastructure/controller/ColorGetController.java
+++ src/main/java/com/ddominguezh/superhero/app/color/infrastructure/controller/ColorGetController.java
@@ -2,10 +2,8 @@ package com.ddominguezh.superhero.app.color.infrastructure.controller;
 
 import java.util.HashMap;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
