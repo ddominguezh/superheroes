7fe4d28c3687c485dcc1b125da15ca2f35633dbe
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:35:02 +0100

    finish complete class

--- src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/FindGenderResponse.java
+++ src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/FindGenderResponse.java
@@ -1,19 +1,32 @@
 package com.ddominguezh.superhero.app.gender.application.useCase.findGender;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.ddominguezh.superhero.app.gender.domain.Gender;
 import com.ddominguezh.superhero.shared.domain.bus.query.Response;
 
 public class FindGenderResponse implements Response {
 
+	private List<GenderResponse> genders;
+	
+	public FindGenderResponse(List<GenderResponse> genders) {
+		this.genders = genders;
+	}
+
+	public static FindGenderResponse from(List<Gender> genders) {
+		return new FindGenderResponse(
+				genders.stream()
+					.map(gender -> GenderResponse.from(gender))
+					.collect(Collectors.toList())
+			);
+	}
+
+	public List<GenderResponse> getGenders(){
+		return this.genders;
+	}
+	
 	public Integer getSize() {
-		return 0;
+		return this.genders.size();
 	}
-
-	public static FindGenderResponse from(List<Gender> findAll) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
