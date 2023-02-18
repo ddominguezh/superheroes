86e92e2f87f97f0e5613c499f4eb20e2c69ab052
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 19:28:11 +0100

    parse domain model to response

--- src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/findOneHero/FindOneHeroResponse.java
+++ src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/findOneHero/FindOneHeroResponse.java
@@ -1,10 +1,19 @@
 package com.ddominguezh.superhero.app.hero.application.useCase.findOneHero;
 
+import com.ddominguezh.superhero.app.hero.domain.Hero;
 import com.ddominguezh.superhero.shared.domain.bus.query.Response;
 
 public class FindOneHeroResponse implements Response {
 
 	private String id;
+	
+	FindOneHeroResponse(String id){
+		this.id = id;
+	}
+
+	public static FindOneHeroResponse from(Hero hero) {
+		return new FindOneHeroResponse(hero.id());
+	}
 	
 	public String getId() {
 		return this.id;
