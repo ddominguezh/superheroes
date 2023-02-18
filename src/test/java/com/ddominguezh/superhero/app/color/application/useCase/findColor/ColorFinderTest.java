0cdec3623b4ccf6bbf69a0e1710c12a8857f2f1c
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 09:09:51 +0100

    add get_all_colors test

--- src/test/java/com/ddominguezh/superhero/app/color/application/useCase/findColor/ColorFinderTest.java
+++ src/test/java/com/ddominguezh/superhero/app/color/application/useCase/findColor/ColorFinderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ddominguezh.superhero.app.color.domain.Color;
+import com.ddominguezh.superhero.app.color.domain.ColorMother;
 import com.ddominguezh.superhero.app.color.domain.repository.ColorRepository;
 import com.ddominguezh.superhero.shared.SuperheroApplication;
 
@@ -40,5 +42,19 @@ public class ColorFinderTest {
 		when(repository.findAll()).thenReturn(null);
 		FindColorResponse response = finder.invoke(new FindColorQuery());
 		assertEquals(0, response.getSize());
+	}
+	
+	@Test
+	public void get_all_colors() {
+		List<Color> colors = ColorMother.randomColors();
+		when(repository.findAll()).thenReturn(colors);
+		FindColorResponse response = finder.invoke(new FindColorQuery());
+		assertEquals(colors.size(), response.getSize());
+		for(int i = 0 ; i < response.getSize() ; i++ ) {
+			Color color = colors.get(i);
+			ColorResponse colorResponse = response.getColors().get(i);
+			assertEquals(color.getId(), colorResponse.getId());
+			assertEquals(color.getName(), colorResponse.getName());
+		}
 	}
 }
