4853a6d4ebd1df97300655d479ca7f91041248fb
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 07:56:00 +0100

    Create color endpoint

---
+++ src/main/java/com/ddominguezh/superhero/app/color/infrastructure/controller/ColorGetController.java
@@ -0,0 +1,44 @@
+package com.ddominguezh.superhero.app.color.infrastructure.controller;
+
+import java.util.HashMap;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ddominguezh.superhero.app.color.applicattion.useCase.findColor.FindColorResponse;
+import com.ddominguezh.superhero.shared.domain.DomainError;
+import com.ddominguezh.superhero.shared.domain.bus.command.CommandBus;
+import com.ddominguezh.superhero.shared.domain.bus.query.QueryBus;
+import com.ddominguezh.superhero.shared.infrastructure.controller.ApiController;
+
+@Controller(value="ColorGetController")
+@RequestMapping("color")
+public class ColorGetController extends ApiController{
+
+	public ColorGetController(QueryBus queryBus, CommandBus commandBus) {
+		super(queryBus, commandBus);
+	}
+
+	@CrossOrigin
+	@RequestMapping(
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE
+		)
+	@ResponseBody
+	public FindColorResponse index() throws Exception{
+		return new FindColorResponse();
+	}
+
+	@Override
+	public HashMap<Class<? extends DomainError>, HttpStatus> errorMapping() {
+		return new HashMap<Class<? extends DomainError>, HttpStatus>();
+	}
+	
+}
