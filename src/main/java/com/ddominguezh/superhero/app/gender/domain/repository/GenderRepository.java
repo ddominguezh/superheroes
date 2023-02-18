263ca19bf2834c04f252201ccb9dbf7988d5336e
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:16:22 +0100

    create find all

--- src/main/java/com/ddominguezh/superhero/app/gender/domain/repository/GenderRepository.java
+++ src/main/java/com/ddominguezh/superhero/app/gender/domain/repository/GenderRepository.java
@@ -1,5 +1,11 @@
 package com.ddominguezh.superhero.app.gender.domain.repository;
 
+import java.util.List;
+
+import com.ddominguezh.superhero.app.gender.domain.Gender;
+
 public interface GenderRepository {
 
+	List<Gender> findAll();
+	
 }
