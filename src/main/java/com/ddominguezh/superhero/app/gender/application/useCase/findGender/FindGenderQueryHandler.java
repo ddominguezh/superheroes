79c966c0c3d0659dd2bb305e53dfa372ba100128
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 18:41:58 +0100

    create query hander

---
+++ src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/FindGenderQueryHandler.java
@@ -0,0 +1,20 @@
+package com.ddominguezh.superhero.app.gender.application.useCase.findGender;
+
+import javax.annotation.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ddominguezh.superhero.shared.domain.bus.query.QueryHandler;
+
+@ManagedBean
+public class FindGenderQueryHandler implements QueryHandler<FindGenderQuery, FindGenderResponse>{
+
+	@Autowired
+	private GenderFinder finder;
+	
+	@Override
+	public FindGenderResponse handle(FindGenderQuery query) {
+		return finder.invoke(query);
+	}
+
+}
