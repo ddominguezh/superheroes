c2f2fa70cc0cf166afea3d9a285c211cb4afa7c8
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 19:27:49 +0100

    call to repository

--- src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/findOneHero/OneHeroFinder.java
+++ src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/findOneHero/OneHeroFinder.java
@@ -1,13 +1,19 @@
 package com.ddominguezh.superhero.app.hero.application.useCase.findOneHero;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ddominguezh.superhero.app.hero.domain.repository.HeroRepository;
+import com.ddominguezh.superhero.app.hero.domain.valueObject.HeroId;
 
 @Service
 public class OneHeroFinder {
 
+	@Autowired
+	private HeroRepository repository;
+	
 	public FindOneHeroResponse invoke(FindOneHeroQuery query) {
-		// TODO Auto-generated method stub
-		return null;
+		return FindOneHeroResponse.from(this.repository.findById(HeroId.create(query.getId())));
 	}
 
 }
