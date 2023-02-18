d8a8ddc40955adf6b4dabb8acc6015ab6c5c8228
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 19:56:59 +0100

    create ko tests

--- src/test/java/com/ddominguezh/superhero/app/hero/infrastructure/repository/H2HeroRepositoryTest.java
+++ src/test/java/com/ddominguezh/superhero/app/hero/infrastructure/repository/H2HeroRepositoryTest.java
@@ -1,5 +1,41 @@
 package com.ddominguezh.superhero.app.hero.infrastructure.repository;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ddominguezh.superhero.app.hero.domain.Hero;
+import com.ddominguezh.superhero.app.hero.domain.valueObject.HeroId;
+import com.ddominguezh.superhero.shared.SuperheroApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = SuperheroApplication.class)
 public class H2HeroRepositoryTest {
 
+	@Autowired
+	private H2HeroRepository repository;
+	
+	@Test
+	public void hero_detail() {
+		String heroId = "a8e012a9-d074-4f86-a927-125e25f13c6d";
+		Optional<Hero> hero = repository.findById(HeroId.create(heroId));
+		assertTrue(hero.isPresent());
+		assertEquals(heroId, hero.get().id());
+	}
+	
+	@Test
+	public void hero_not_found() {
+		Optional<Hero> hero = repository.findById(HeroId.create(UUID.randomUUID().toString()));
+		assertTrue(hero.isEmpty());
+	}
 }
