f740ad3e95071cb0df6ba835d96cf5a09850daa5
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 17:28:54 +0100

    create function parse model to response

--- src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/FindGenderResponse.java
+++ src/main/java/com/ddominguezh/superhero/app/gender/application/useCase/findGender/FindGenderResponse.java
@@ -1,11 +1,19 @@
 package com.ddominguezh.superhero.app.gender.application.useCase.findGender;
 
+import java.util.List;
+
+import com.ddominguezh.superhero.app.gender.domain.Gender;
 import com.ddominguezh.superhero.shared.domain.bus.query.Response;
 
 public class FindGenderResponse implements Response {
 
 	public Integer getSize() {
 		return 0;
+	}
+
+	public static FindGenderResponse from(List<Gender> findAll) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
