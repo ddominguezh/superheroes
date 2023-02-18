de6b05358d12525b042f6a1f18fb6b5b92edb12b
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 21:05:02 +0100

    add ping test

---
+++ src/test/java/com/ddominguezh/superhero/app/hero/infrastructure/controller/HeroDeleteControllerTest.java
@@ -0,0 +1,36 @@
+package com.ddominguezh.superhero.app.hero.infrastructure.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ddominguezh.superhero.shared.SuperheroApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = SuperheroApplication.class)
+@AutoConfigureMockMvc
+@Transactional
+public class HeroDeleteControllerTest {
+
+	private static final String heroId = "7f675eca-afbf-11ed-afa1-0242ac120002";
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void ping_hero_enpoint() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.delete("/hero/" + heroId)
+				.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+}
