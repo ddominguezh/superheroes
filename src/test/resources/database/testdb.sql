d88d6173d08c78873436d03f718ef17a46592fd0
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 18:02:52 +0100

    create gender table and default inserts

--- src/test/resources/database/testdb.sql
+++ src/test/resources/database/testdb.sql
@@ -7,3 +7,11 @@ insert into color( id, name ) values (3, 'yellow');
 insert into color( id, name ) values (4, 'black');
 insert into color( id, name ) values (5, 'white');
 insert into color( id, name ) values (6, 'red');
+
+DROP TABLE IF EXISTS gender;
+CREATE TABLE gender(ID INT PRIMARY KEY,
+    NAME VARCHAR(20) DEFAULT '' NOT NULL);
+insert into gender( id, name ) values (1, 'UNDEFINED');
+insert into gender( id, name ) values (2, 'WOMEN');
+insert into gender( id, name ) values (3, 'MEN');
+insert into gender( id, name ) values (4, 'TRANSGENDER');
