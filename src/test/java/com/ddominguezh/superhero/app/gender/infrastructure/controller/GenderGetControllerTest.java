19de95032c17dc84073b2c99b3693d485037bf33
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 18:29:27 +0100

    create get_all_genders test

--- src/test/java/com/ddominguezh/superhero/app/gender/infrastructure/controller/GenderGetControllerTest.java
+++ src/test/java/com/ddominguezh/superhero/app/gender/infrastructure/controller/GenderGetControllerTest.java
@@ -1,5 +1,10 @@
 package com.ddominguezh.superhero.app.gender.infrastructure.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +15,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.ddominguezh.superhero.app.gender.application.useCase.findGender.FindGenderResponse;
+import com.ddominguezh.superhero.app.gender.application.useCase.findGender.GenderResponse;
+import com.ddominguezh.superhero.app.gender.domain.Gender;
 import com.ddominguezh.superhero.shared.SuperheroApplication;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,4 +41,30 @@ public class GenderGetControllerTest {
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 	
+	@Test
+	public void get_all_genders() throws Exception {
+		String response = mockMvc.perform(MockMvcRequestBuilders.get("/gender")
+				.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		FindGenderResponse genders = gson.fromJson(response, FindGenderResponse.class);
+		
+		List<GenderResponse> expected = new ArrayList<GenderResponse>() {
+			{
+				add(GenderResponse.from(Gender.create(1, "UNDEFINED")));
+				add(GenderResponse.from(Gender.create(2, "WOMEN")));
+				add(GenderResponse.from(Gender.create(3, "MEN")));
+				add(GenderResponse.from(Gender.create(4, "TRANSGENDER")));
+			}
+		};
+		
+		assertEquals(expected.size(), genders.getSize());
+		for(int i = 0 ; i < expected.size() ; i++) {
+			assertEquals(expected.get(i), genders.getGenders().get(i));
+		}
+	}
 }
