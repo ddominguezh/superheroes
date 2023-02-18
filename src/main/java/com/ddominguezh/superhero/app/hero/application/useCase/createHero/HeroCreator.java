0b343fdb26fafc137da0c541bc5b7e3eb26c8c03
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-18 22:38:43 +0100

    call the create function of the repository

--- src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/createHero/HeroCreator.java
+++ src/main/java/com/ddominguezh/superhero/app/hero/application/useCase/createHero/HeroCreator.java
@@ -3,6 +3,7 @@ package com.ddominguezh.superhero.app.hero.application.useCase.createHero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ddominguezh.superhero.app.hero.domain.Hero;
 import com.ddominguezh.superhero.app.hero.domain.repository.HeroRepository;
 
 @Service
@@ -12,6 +13,6 @@ public class HeroCreator {
 	private HeroRepository repository;
 	
 	public void invoke(CreateHeroCommand command) {
-		
+		repository.create(Hero.create(command.getId(), command.getName()));
 	}
 }
