5233ef4dc9e67ddf1d7c228a940d868e8d8dac8a
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-19 16:37:34 +0100

    add security tests

---
+++ src/test/java/com/ddominguezh/superhero/shared/domain/AuthorizationTest.java
@@ -0,0 +1,24 @@
+package com.ddominguezh.superhero.shared.domain;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import com.ddominguezh.superhero.shared.domain.exception.AuthorizationException;
+
+public class AuthorizationTest {
+
+	@Test
+	public void authorized() throws UnsupportedEncodingException {
+		new Authorization("Bearer MTIzYTRkZjgtYjA2OS0xMWVkLWFmYTEtMDI0MmFjMTIwMDAy");
+	}
+	
+	@Test
+	public void unauthorized() {
+		assertThrows(AuthorizationException.class, () -> new Authorization("Bearer " + Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes())));
+	}
+}
