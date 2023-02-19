0662aa49d1e14c40a810bbf8869830027ff10bb6
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-19 09:03:59 +0100

    throw exception when gender not found

--- src/main/java/com/ddominguezh/superhero/app/hero/domain/useCase/findGenderById/GenderByIdFinder.java
+++ src/main/java/com/ddominguezh/superhero/app/hero/domain/useCase/findGenderById/GenderByIdFinder.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ddominguezh.superhero.app.hero.domain.exception.HeroGenderNotFoundException;
 import com.ddominguezh.superhero.app.hero.domain.repository.HeroGenderRepository;
 import com.ddominguezh.superhero.app.hero.domain.valueObject.HeroGender;
 import com.ddominguezh.superhero.app.hero.domain.valueObject.HeroGenderId;
@@ -17,6 +18,9 @@ public class GenderByIdFinder {
 	
 	public HeroGender invoke(HeroGenderId id) {
 		Optional<HeroGender> gender = repository.findById(id);
+		if(gender.isEmpty()) {
+			throw new HeroGenderNotFoundException(id.value());
+		}
 		return gender.get();
 	}
 
