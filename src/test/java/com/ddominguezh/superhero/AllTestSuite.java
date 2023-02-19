36be4c94f74e46ddbd4299e02bb0616866c5038e
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-19 16:37:49 +0100

    add security tests

--- src/test/java/com/ddominguezh/superhero/AllTestSuite.java
+++ src/test/java/com/ddominguezh/superhero/AllTestSuite.java
@@ -1,4 +1,4 @@
-package com.ddominguezh.superhero.app;
+package com.ddominguezh.superhero;
 
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
@@ -7,13 +7,15 @@ import com.ddominguezh.superhero.app.checkHealth.CheckHealthTestSuite;
 import com.ddominguezh.superhero.app.color.ColorTestSuite;
 import com.ddominguezh.superhero.app.gender.GenderTestSuite;
 import com.ddominguezh.superhero.app.hero.HeroTestSuite;
+import com.ddominguezh.superhero.shared.domain.AuthorizationTest;
 
 @SelectClasses(
 		{
 			CheckHealthTestSuite.class,
 			ColorTestSuite.class,
 			GenderTestSuite.class,
-			HeroTestSuite.class
+			HeroTestSuite.class,
+			AuthorizationTest.class
 		}
 	)
 @Suite
