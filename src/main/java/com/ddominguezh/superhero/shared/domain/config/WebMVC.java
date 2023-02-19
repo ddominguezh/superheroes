6dd18fcd1932e92b824e38702430230ffe6fff4e
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-19 17:40:59 +0100

    configure aspectj

--- src/main/java/com/ddominguezh/superhero/shared/domain/config/WebMVC.java
+++ src/main/java/com/ddominguezh/superhero/shared/domain/config/WebMVC.java
@@ -4,12 +4,14 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableAspectJAutoProxy
 @ComponentScan({"com.ddominguezh.superhero.app"})
 public class WebMVC implements WebMvcConfigurer {
 	
