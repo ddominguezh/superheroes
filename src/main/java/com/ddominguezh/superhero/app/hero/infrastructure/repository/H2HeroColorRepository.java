fe83ebcd38a9b8474e8d6baa24540e1fa8c6c64a
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-02-19 10:42:48 +0100

    call hibernate repository

--- src/main/java/com/ddominguezh/superhero/app/hero/infrastructure/repository/H2HeroColorRepository.java
+++ src/main/java/com/ddominguezh/superhero/app/hero/infrastructure/repository/H2HeroColorRepository.java
@@ -2,19 +2,27 @@ package com.ddominguezh.superhero.app.hero.infrastructure.repository;
 
 import java.util.Optional;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ddominguezh.superhero.app.hero.domain.repository.HeroColorRepository;
 import com.ddominguezh.superhero.app.hero.domain.valueObject.HeroColor;
 import com.ddominguezh.superhero.app.hero.domain.valueObject.HeroColorId;
+import com.ddominguezh.superhero.shared.infrastructure.hibernate.HibernateRepository;
 
 @Repository
-public class H2HeroColorRepository implements HeroColorRepository {
+@Transactional("transaction_manager")
+public class H2HeroColorRepository extends HibernateRepository<HeroColor> implements HeroColorRepository {
+
+	public H2HeroColorRepository(@Qualifier("session_factory") SessionFactory sessionFactory) {
+		super(sessionFactory, HeroColor.class);
+	}
 
 	@Override
 	public Optional<HeroColor> findById(HeroColorId id) {
-		// TODO Auto-generated method stub
-		return null;
+		return super.byId(id);
 	}
 
 }
